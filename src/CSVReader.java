33754c09843d64cb0009249a38284e0ef8dfe3d7
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2023-09-22 14:13:49 +0200

    second commit

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -1,13 +1,36 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CSVReader {
     
-    public double[][] readDoubleCSV(String path) throws FileNotFoundException{
+    public double[][] readDoubleCSV(String path) throws IOException{
         double[][] res = new double[0][0];
         BufferedReader br = new BufferedReader(new FileReader(path));
 
+        ArrayList<ArrayList<Double>> data = new ArrayList<>();
+
+        while(br.ready()){
+            Scanner scanner = new Scanner(br.readLine()).useDelimiter(",");
+            ArrayList<Double> line = new ArrayList<>();
+
+            while(scanner.hasNext()){
+                line.add(scanner.nextDouble());
+            }
+
+            data.add(line);
+
+        }
+
+        for(int i=0;i<data.size()){
+            for(int j=0;j<data.get(i).size()){
+                
+            }
+        }
+
+        br.close();
         return res;
     }
 
